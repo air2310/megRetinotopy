6d26e26b9f8f4b8a5958ed272ae4682ffe1701f6
Barrie Klein <barrieklein@gmail.com>
2017-07-05 14:28:21 +0200

    Updates

--- Devel/mprfSession_visualize_prf_responses.m
+++ Devel/mprfSession_visualize_prf_responses.m
@@ -6,15 +6,19 @@ if isempty(mprfSESSION)
     load('mprfSESSION.mat')
 end
 
+
+main_dir = mprf__get_directory('main_dir');
+anat_dir = mprf__get_directory('bs_anat');
+
 pred = mprf__load_model_predictions;
-
-bs_msh = mprfMeshFromBrainstorm(mprfSESSION.source.(surf.name));
-bs_msh = meshVisualize(bs_msh);
-
 [~,tmp] = fileparts(pred.bs.model_file);
+
 surf_file = [tmp(strfind(tmp,'tess'):end) '.mat'];
+surf_path = fullfile(main_dir, anat_dir, surf_file);
 
 
+bs_msh = mprfMeshFromBrainstorm(surf_path);
+bs_msh = meshVisualize(bs_msh);
 
 
 
