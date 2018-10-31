9c4efbfa96485fe080c7cf4cbf3137f3ace7a17a
elinekupers <elinekupers@gmail.com>
2018-10-31 09:53:15 -0400

    add first version of a new script that initializes a specific subject with local paths instead of absolute paths

--- analysis/s_initSession_wlsubj030.m
+++ analysis/s_initSession_wlsubj030.m
@@ -1,23 +1,53 @@
 %% s_initSession_wlsubj030
 
+% This is a script to define the paths of the files we need to run a model.
+% 
 
 % Define subject and its paths
 subject            = 'wlsubj030';
 dataDir            = '/Volumes/server/Projects/MEG/Retinotopy/Data/';
 vistaSessionDir    = fullfile(dataDir, 'fMRI', subject, 'VistaSession');
-megPreprocessedDir = fullfile(dataDir, 'MEG', subject, 'processed');
-outPutDir          = fullfile('/Volumes/server/Projects/MEG/Subject_sessions', subject);
+megDataDir         = fullfile(dataDir, 'MEG', subject);
+outPutDir          = fullfile(mprfRootPath, 'data', subject);
 
+% Predefine struct
 s = struct();
 
 % Find MEG time series
-s.MEGtimeseries = fullfile(megPreprocessedDir, 'epoched_data_hp_preproc_denoised.mat');
+s.MEGtimeseries = fullfile(megDataDir, 'processed', 'epoched_data_hp_preproc_denoised.mat');
 
 % Find MRI PRF params
-s.PRFParams = fullfile(vistaSessionDir, 'Gray', 'rm_);
+d = dir(fullfile(vistaSessionDir, 'Gray', '*','*fFit*'));
+s.PRFParams.pth = fullfile(d.folder, d.name);
 
+% Find MEG & MRI stimulus (and check if they are the same)
+s.MRIStimIm.pth     = fullfile(vistaSessionDir, 'Stimuli', 'scan_images.mat');
+s.MRIStimParams.pth = fullfile(vistaSessionDir, 'Stimuli', 'scan_params.mat');
+s.MEGStim.pth       = fullfile(megDataDir, 'raw', 'R0942_MegRet_9.11.18','stimFiles', 'MEG_retinotopy_stimulus_run_1.mat');
+s.MEGStimGrid.pth   = fullfile(megDataDir, 'raw', 'R0942_MegRet_9.11.18','stimFiles', 'MEG_grid.mat');
 
-% Find stimulus
+% RM model, with stimulus
+prfParams = load(s.PRFParams.pth);
+
+megStimulus = load(s.MEGStim.pth, 'stimulus');
+megStimulusGrid = load(s.MEGStimGrid.pth);
+
+%   Make grid?
+sz1 = numel(X);
+sz2 = 
+X   = repmat(X(:),1,sz2);
+Y   = repmat(Y(:),1,sz2);
+
+% define centered around (0,0) coordinate space
+x0 = repmat(x0(:)',sz1,1);
+y0 = repmat(y0(:)',sz1,1);
+
+% Translate grid so that center is at RF center
+X = X - x0;   % positive x0 moves center right
+Y = Y - y0;   % positive y0 moves center up
+    
+    
+% check where coordinates fall wrt image?
 
 % Find ROIS in Freesurfer directory
 
