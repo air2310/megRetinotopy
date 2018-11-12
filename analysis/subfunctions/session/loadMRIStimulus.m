24a1e01d82d417acbcc397f66e63341ec50f9ecd
elinekupers <elinekupers@gmail.com>
2018-11-12 18:58:01 -0500

    fix load mri stimulus, using the correct x and y coordinates of stimulus

--- analysis/subfunctions/session/loadMRIStimulus.m
+++ analysis/subfunctions/session/loadMRIStimulus.m
@@ -11,8 +11,11 @@ yRange         = [];
 % Get MRI prf scan parameters from file
 prfParams = load(stimPths.PRFParams.pth);
 
-% [EK]: not sure what the difference is between unconvolved and all
-% stim images. And why we even need this?
+% [EK]: Apparently we only take the pixels that fall within the square
+% window mask (im_conv). We don't need the im_unconv and we also don't need
+% the allX and allY or allim for any computations (like smoothing the
+% parameters on the surface). I guess it is good to have for
+% visualization??
 retStim.im_unconv = prfParams.params.stim.images_unconvolved;
 retStim.im_conv = prfParams.params.analysis.allstimimages';
 
@@ -21,9 +24,10 @@ X = prfParams.params.analysis.X;
 Y = prfParams.params.analysis.Y;
 
 % Create X and Y axis of stimulus grid
-[retStim.X, retStim.Y] = meshgrid(unique(X), unique(Y));
-
+[retStim.allX, retStim.allY] = meshgrid(unique(X), unique(Y));
+retStim.X = X;
+retStim.Y = Y;
 % Get final retinotopy stimulus images (previously called full_im)
-[~, retStim.im] = rmStimulusMatrix(prfParams.params,xRange,yRange,useFinalImages,scans);
+[~, retStim.allim] = rmStimulusMatrix(prfParams.params,xRange,yRange,useFinalImages,scans);
 
 end
