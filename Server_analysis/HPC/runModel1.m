8cfe44c832bc939ff7ab823b31d10233ec90d690
elinekupers <elinekupers@gmail.com>
2018-11-14 16:28:11 -0500

    bugfix get meg file

--- Server_analysis/HPC/runModel1.m
+++ Server_analysis/HPC/runModel1.m
@@ -3,7 +3,7 @@ function runModel1()
     sub_sess_dir = pwd; 
     save_path = fullfile(sub_sess_dir, 'modeling', 'results');
     d = dir(fullfile(sub_sess_dir, 'data', 'meg', 'preproc', '*', 'epoched_data_hp_preproc_denoised.mat'));    
-    meg_data_file = fullfile(d(1).folder, d(1).name); 
+    meg_data_file = fullfile(d.folder, d.name); 
     mprfSession_model_server(save_path,meg_data_file,sub_sess_dir,'1'); 
 
 end
