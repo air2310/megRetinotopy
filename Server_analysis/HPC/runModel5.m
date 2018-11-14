867bb834f38d00d1584fcff9f980038a4ac6e310
elinekupers <elinekupers@gmail.com>
2018-11-14 18:27:09 -0500

    add data path in bash script and add other model scripts for hpc

---
+++ Server_analysis/HPC/runModel5.m
@@ -0,0 +1,9 @@
+function runModel5()
+
+    sub_sess_dir = pwd; 
+    save_path = fullfile(sub_sess_dir, 'modeling', 'results');
+    d = dir(fullfile(sub_sess_dir, 'data', 'meg', 'preproc', '*', 'epoched_data_hp_preproc_denoised.mat'));    
+    meg_data_file = fullfile(d.folder, d.name); 
+    mprfSession_model_server(save_path,meg_data_file,sub_sess_dir,'5'); 
+
+end
