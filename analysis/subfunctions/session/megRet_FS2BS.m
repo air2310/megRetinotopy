5649e652ebd7bd18ba1a08bfca8cf2926a77bf75
elinekupers <elinekupers@gmail.com>
2018-11-14 20:16:54 -0500

    updates on fs to bs scripts

--- analysis/subfunctions/session/megRet_FS2BS.m
+++ analysis/subfunctions/session/megRet_FS2BS.m
@@ -5,253 +5,221 @@ if ~exist('MRIread')
     addpath(genpath('/Applications/freesurfer/fsfast/toolbox'));
 end
 
+% open brainstorm
+brainstorm
+
 % Load the brainstorm head model file, as we need to know on which surface
 % it is defined:
 % bsOverlay = load(fullfile(s.BS.surface.pth, 'tess_cortex_pial_low.mat'));
 
 % Set the freesurfer subject dir
 fsSubjDir = fullfile(s.freeSurferDir, s.fsSubject);
-setenv('SUBJECT_DIR', s.freeSurferDir)
+setenv('SUBJECTS_DIR', s.freeSurferDir)
 
 % Set the brainstorm subject dir
 bsSubjDir = s.BS.surface.pth;
 
 % Save ROIs
-s.saveRoiDir = fullfile(s.outPut.pth, s.fsSubject, 'BSrois');
-if ~exist(s.saveDir,'dir'); mkdir(s.saveDir); end;
+saveRoiDir = fullfile(s.outPut.pth, 'rois');
+if ~exist(saveRoiDir,'dir'); mkdir(saveRoiDir); end;
 
 % Get the Wang Atlas
-wangAtlas_fname = @(hem, type)(sprintf('%s/surf/%s.wang2015_atlas.mgz', fsSubjDir, hem));
+prfParamfname = @(hem, type)(sprintf('%s/surf/%s.wang2015_atlas.mgz', fsSubjDir, hem));
 surfdat         = @(lh, rh, rc)(setfield(setfield([], 'lh', lh.vol(:)), 'rh', rc*rh.vol(:)));
 
-subFSWang       = surfdat(MRIread(wangAtlas_fname('lh')), MRIread(wangAtlas_fname('rh')), 1);
+subFSWang       = surfdat(MRIread(prfParamfname('lh')), MRIread(prfParamfname('rh')), 1);
 
 switch roiType
     
-    case 'allWangROIs'
-    
+    case 'allRoisWangAtlas'
+        
         % get all indiv
         lh.ROI_data = subFSWang.lh>0;
         rh.ROI_data = subFSWang.rh>0;
-
+        
         % do interpolation
-        wangROI_BS = tess_fs2bst(bsSubjDir, fsSubjDir, lh.ROI_data, rh.ROI_data);
-
+        wangROI_BS = tess_fs2bst(s.bsSubject, fsSubjDir, lh.ROI_data, rh.ROI_data);
+        
         % Create file names for interpolated data
-        subBSAllROImgzfile = sprintf('%s/allWangROIs_overlay.mgz', s.saveRoiDir);
-        subBSAllROImatfile = sprintf('%s/allWangROIs_overlay.mat', s.saveRoiDir);
-
+        subBSAllROImgzfile = sprintf('%s/allWangROIs_overlay.mgz', saveRoiDir);
+        subBSAllROImatfile = sprintf('%s/allWangROIs_overlay.mat', saveRoiDir);
+        
         % save ROIs as mgz file
         MRIwrite(struct('vol', wangROI_BS), subBSAllROImgzfile);
-
+        
         % save ROIs as mat file
         save(subBSAllROImatfile, 'wangROI_BS');
-
+        
     case 'individual'
         % in case of individual rois???
         
 end
+
+%% Separate PRF data into Left and Right Hemi, then project to FS mesh?
+
+% location of smoothed prf data
+prfDataDir = fullfile(s.outPut.pth, 'prfParams');
+
+load(fullfile(prfDataDir, 'allPRFParams.mat'), 'prfParamsExp');
+
+% Get all the parameters stored in the prf parameter data file:
+parNames = fieldnames(prfParamsExp);
+
+% Keep track of the X and Y variables, both smoothed and unsmoothed to
+% compute the eccenricity and polar angle maps as well:
+has_x = false;
+has_y = false;
+has_x_sm = false;
+has_y_sm = false;
+
+% surfaces to transform from FS to BS
+fsSurfaces = {'lh.pial','rh.pial'};
+
+currPath = pwd;
+cd(s.vistaSession.pth)
+hvol = initHiddenGray;
+mmPerVox = viewGet(hvol,'mmpervox');
+
+% Loop over the FS surfaces:
+for n = 1:length(fsSurfaces)
+    
+    currentSurface = fullfile(s.freeSurferDir, s.fsSubject, 'surf', fsSurfaces{n});
+    tmp = strsplit(fsSurfaces{n},'.');
+    currentHemi = tmp{1};
+    fprintf('Exporting parameters for %s hemisphere:\n',currentHemi)
+    
+    % Load mesh using fs_meshFromSurface, this creates a mrVista compatible
+    % mesh. Using 'my own' function that skips the smoothing:
+    mrVistaMesh = mprf_fs_meshFromSurface(currentSurface);
+    fnum = numel(mrVistaMesh.triangles);
+    
+    % compute mapping using mrmMapVerticesToGray (mrmMapGrayToVertices):
+    currentVista2Graymap = mrmMapVerticesToGray(mrVistaMesh.vertices, viewGet(hvol,'nodes'),...
+        mmPerVox);
+    
+    goodVerticesForMapping = currentVista2Graymap > 0;
+    
+    % cur_v2gmap is really just an index variable into the gray nodes
+    % that tells which gray node is closest to which mesh node. As it uses
+    % nearpoints (i.e. nearest neighbour interpolation), I suspect this only
+    % works for mrVista based meshes.
+    % Would be nice to do something more intelligent than nearest neighbour
+    % interpolation, like checking other layers of the Gray graph as well.
+    
+    % And use the cur_v2gmap to select the pRF parameters of the Gray nodes
+    % that are closest to the mesh nodes/vertices. However, cur_v2gmap contains
+    % zeros, so we need to deal with them:
+    
+    % Loop over the parameters:
+    for nn = 1:length(parNames)
         
-
-% AND NOW the same for PRF DATA
-
-
-
-
-% 
-% 
-% 
-% %%
-% for n = 1:length(surfaces_to_load)
-%     % Get brainstorm mesh
-%     fs_vertices = [];
-%     
-%     % Loop over all (2, left and right) surfaces:
-%     for nn = 1:length(hs_to_load)
-%         cur_surf = [hs_to_load{nn} '.' surfaces_to_load{n}];
-%         surf_file = fullfile(main_dir,mprf__get_directory('fs_surface'),cur_surf);
-%         
-%         % Use the same routine as Brainstorm uses, otherwise the vertices 
-%         % are slightly off and intersectCols does not find all the matching 
-%         % vertices.
-%         
-%         [tmp_verts, ~] = mne_read_surface(surf_file);
-%         
-%          % Transformations applied by brainstorm:
-%         tmp_verts = bsxfun(@plus, tmp_verts, [128 129 128] / 1000);  
-%         tmp_verts = [tmp_verts'; ones(1,size(tmp_verts,1))]; %% Actually verts)
-%         tmp_verts = [bs_mri.SCS.R, bs_mri.SCS.T./1000; 0 0 0 1] * tmp_verts;
-%         tmp_verts = tmp_verts(1:3,:)';
-%         
-%         
-%         % combine left and right hemi
-%         verts = [fs_vertices; tmp_verts];  
-%     end
-%      
-%     
-% %         fs_verticesAligned = cs_convert(bs_mri, 'mri', 'scs', fs_vertices);
-% 
-%         
-%     
-%     % Get brainstorm mesh
-%     surf_path = fullfile(main_dir, mprf__get_directory('bs_anat'));
-%   
-%     % Load the brainstorm surface file:
-%     bs_surf_files = dir(fullfile(surf_path,'*tess_cortex*.mat'));
-%     this_bs_file = find(~cellfun(@isempty,strfind({bs_surf_files.name},surfaces_to_load{n})));
-%     
-%     if isempty(this_bs_file)
-%         error('Could not find Brainstorm surface file for %s',surfaces_to_load{n})
-%     end
-%     load(fullfile(surf_path,bs_surf_files(this_bs_file).name),'Vertices', 'Faces');
-%     
-%     % Find the vertices that we computed in verts in the Vertices generated
-%     % by Brainstorm. Brainstorm downsamples the surfaces (15002 vertices)
-%     % using Matlab's reduce_patch function. This routine does not change
-%     % anything about the vertices, but just selects the most informative
-%     % ones. Therefore, intersectCols should be able to find all 15002
-%     % vertices of the brainstorm surface in our own verts variable. If not,
-%     % possibly something went wrong in the transformations above.
-% %     [~, bs_vert_idx, bs_vert_idx2] = intersectCols(verts', Vertices');
-%     
-%     bs_vert_idx = dsearchn(verts, Vertices);
-%     bs_vert_idx2 = dsearchn(Vertices, verts);
-%     
-%     if numel(bs_vert_idx) == size(Vertices,1);
-%         
-%     else
-%         verts_02 = round(verts ./ 10.^-10) .* 10.^-10;
-%         Vertices_02 = round(Vertices ./ 10.^-10) .* 10.^-10;
-%         
-%         [~, bs_vert_idx, bs_vert_idx2] = intersectCols(verts_02', Vertices_02');
-%         if numel(bs_vert_idx) == size(Vertices,1);
-%             
-%         else
-%             
-%             error('Could only find %d of %d brainstorm vertices in transformed FreeSurfer vertices',...
-%                 length(bs_vert_idx), size(Vertices,1));
-%         end
-%     end
-%     
-%     % Export prfs or rois?
-%     if strcmpi(w_export,'prf')
-%         pname = mprf__get_directory('fs_surf_prf_data');
-% 
-%     elseif strcmpi(w_export,'roi')
-%         pname = mprf__get_directory('fs_surf_roi');
-% 
-%         
-%     end
-%     
-%     lh_files = dir(fullfile(pname,'lh.*'));
-%     % Loop over the LHs files (i.e. all the parameters on the lh surfaces):
-%     for nn = 1:length(lh_files)
-%         
-%         cur_lh_file = lh_files(nn).name;
-%         [~,~,par_name] = fileparts(cur_lh_file);
-%         if strcmpi(par_name,'.mgz')
-%             
-%             
-%         else
-%             par_name = par_name(2:end);
-%             % Find the corresponding rh file:
-%             cur_rh_file = ['rh.' par_name];
-%             
-%             % load and concatenate:
-%             both_data = [read_curv(fullfile(pname,cur_lh_file));...
-%                 read_curv(fullfile(pname,cur_rh_file))];
-%             
-%             % preallocate the output variable:
-% %             both_bs_data_out = nan(size(bs_vert_idx));
-% %             both_bs_data_out = nan(size(bs_vert_idx));
-%             
-%             % Select to correct parameters:
-% %             both_bs_data_out(bs_vert_idx) = both_data(bs_vert_idx);
-%             both_bs_data_out = both_data(bs_vert_idx);
-% 
-%             
-%             % Store the results:
-%             cur_out_file = [surfaces_to_load{n} '.' par_name];
-%             
-%             if strcmpi(w_export,'prf')
-%                 fname = fullfile(mprf__get_directory('bs_surf_prf_data'),cur_out_file);
-%                 
-%             elseif strcmpi(w_export,'roi')
-%                 fname = fullfile(mprf__get_directory('bs_surf_roi'),cur_out_file);
-%                 
-%             end
-%             
-%             write_curv(fname,both_bs_data_out,1);
-%             fprintf('%s\n',cur_out_file);
-%         end
-%         
-%     end
-%     
-%     
-%     
-% end
-% 
-% fprintf('Done.\n')
-% 
-% if strcmpi(w_export,'roi')
-%     check_dir = 'bs_surf_prf_data';
-%     
-%     
-% elseif strcmpi(w_export,'prf')
-%     check_dir = 'bs_surf_roi';
-%     
-% end
-% 
-% 
-% if ~isempty(dir(fullfile(main_dir, mprf__get_directory(check_dir))))
-%     mprfSESSION.has.(check_dir) = true;
-%     
-% else
-%     mprfSESSION.has.(check_dir) = false;
-%     
-% end
-% 
-% cd(main_dir);
-% save(fullfile(main_dir,'mprfSESSION'),'mprfSESSION')
-% mrvCleanWorkspace;
-% clear mprfSESSION
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-
-
-
+        currentParameterName = parNames{nn};
+        tmp = nan(size(currentVista2Graymap));
+        
+        if sum(double(currentParameterName)) == sum(double('beta'))
+            tmp_data = squeeze(prfParamsExp.(currentParameterName)(:,:,1));
+            tmp(goodVerticesForMapping) = tmp_data(currentVista2Graymap(goodVerticesForMapping));
+            
+        else
+            tmp(goodVerticesForMapping) = prfParamsExp.(currentParameterName)(currentVista2Graymap(goodVerticesForMapping));
+            
+        end
+        % Check if we have x, y, x_smoothed or y_smoothed:
+        if regexp(currentParameterName,'x\>');
+            has_x = true;
+            x_pos = tmp;
+            disp(currentParameterName)
+        elseif regexp(currentParameterName,'y\>');
+            has_y = true;
+            y_pos = tmp;
+            disp(currentParameterName)
+        elseif regexp(currentParameterName,'x_smoothed\>');
+            has_x_sm = true;
+            x_pos_sm = tmp;
+            disp(currentParameterName)
+        elseif regexp(currentParameterName,'y_smoothed\>');
+            has_y_sm = true;
+            y_pos_sm = tmp;
+            disp(currentParameterName)
+            
+            
+        end
+        % If we have the necessary data to compute polar angle and
+        % eccentricity, to that as well and store the results:
+        if has_x && has_y
+            [tmp_ang, tmp_ecc] = cart2pol(x_pos, y_pos);
+            
+            fname = fullfile(prfDataDir,[currentHemi '.polar_angle']);
+            write_curv( fname,tmp_ang, fnum);
+            fprintf('Polar angle\n')
+            
+            fname = fullfile(prfDataDir,[currentHemi '.eccentricity']);
+            write_curv( fname,tmp_ecc, fnum);
+            fprintf('Eccentricity\n')
+            has_x = false;
+            has_y = false;
+            
+        end
+        
+        % Same for smoothed versions:
+        if has_x_sm && has_y_sm
+            [tmp_ang, tmp_ecc] = cart2pol(x_pos_sm, y_pos_sm);
+            
+            
+            fname = fullfile(prfDataDir,[currentHemi '.polar_angle_smoothed']);
+            write_curv( fname,tmp_ang, fnum);
+            fprintf('Polar angle smoothed\n')
+            
+            
+            fname = fullfile(prfDataDir,[currentHemi '.eccentricity_smoothed']);
+            write_curv( fname,tmp_ecc, fnum);
+            fprintf('Eccentricity smoothed\n')
+            
+            has_x_sm = false;
+            has_y_sm = false;
+            
+        end
+        % output the results:
+        fname = fullfile(prfDataDir,[currentHemi '.' currentParameterName]);
+        write_curv(fname,tmp, fnum);
+        fprintf('%s\n', currentParameterName)
+        
+    end
+    
 end
 
+cd(currPath)
+
+%% Project to FS data back to brainstorm..
+% (we could've gone straight from the smoothing into a mgz file. not sure why this is done this way...)
+
+prfParamsToUse = {'beta', 'eccentricity', 'eccentricity_smoothed', 'mask', ...
+    'mresp', 'mresp_smoothed', 'polar_angle', 'polar_angle_smoothed', 'recomp_beta', ...
+    'sigma', 'sigma_smoothed', 'varexplained', 'x', 'x_smoothed', 'y', 'y_smoothed'};
+
+% Loop over the param files listed
+for nn = 1:length(prfParamsToUse)
+    
+    thisParam = prfParamsToUse{nn};
+    % Get the prf surface
+    prfParamfname = @(hem)(sprintf('%s/%s.%s', prfDataDir, hem, thisParam));
+    
+    surfdataFS.lh = read_curv(prfParamfname('lh')); 
+    surfdataFS.rh = read_curv(prfParamfname('rh'));
+
+
+    % do interpolation
+    surfdataBS = tess_fs2bst(s.bsSubject, fsSubjDir, surfdataFS.lh, surfdataFS.rh);
+        
+    % Create file names for interpolated data
+    surfdataBSfile = sprintf('%s/%s.%s', prfDataDir, 'pial', thisParam);
+ 
+    % save ROIs as mgz file
+    write_curv(surfdataBSfile,surfdataBS,1);
+end  
+
+    
+    
 
 
 
