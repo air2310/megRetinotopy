230482a570c24c3ac650ac6c1e9af5d2ee848681
elinekupers <elinekupers@gmail.com>
2018-11-14 20:18:52 -0500

    add periods for wlsubj030

--- Server_analysis/mprfSession_run_prf_size_range_model_server.m
+++ Server_analysis/mprfSession_run_prf_size_range_model_server.m
@@ -47,9 +47,19 @@ if ~exist('data','var') || isempty(data)
     
     cd(cur_dir)
 end
-periods.blank = [3:5 30:32 57:59 84:86 111:113 138:140];
-periods.blink = [1 2 28 29 55 56 82 83 109 110 136 137];
-periods.stim = setdiff(1:140,[periods.blink periods.blank]);
+
+[~, subjectName] = fileparts(fileparts(fileparts(fileparts(fileparts(fileparts(meg_data_file_path))))));
+if strcmp(subjectName, 'wlsubj030')
+    preproc_dir = fileparts(meg_data_file_path);
+    load(fullfile(preproc_dir,'megStimConditions.mat'),'designConditions');
+    periods.blink = find(designConditions==10);
+    periods.blank = find(designConditions==20);
+    periods.stim  = find(designConditions<9);
+else
+    periods.blank = [3:5 30:32 57:59 84:86 111:113 138:140];
+    periods.blink = [1 2 28 29 55 56 82 83 109 110 136 137];
+    periods.stim = setdiff(1:140,[periods.blink periods.blank]);
+end
 
 sz = size(data.data);
 opts.n_time = sz(1);
