7050d2d91d05191b379512c6716f18b2ece1ccce
elinekupers <elinekupers@gmail.com>
2019-02-17 17:16:36 -0500

    add to more masks

--- analysis/subfunctions/session/s_visualizeROIvsVEmask.m
+++ analysis/subfunctions/session/s_visualizeROIvsVEmask.m
@@ -5,6 +5,11 @@ m.roi = tmp.roi.mask;
 tmp = load('ve_mask.mat');
 m.ve = tmp.cur_in;
 
+tmp = load('roimask_noroimask.mat');
+m.all = tmp.roi.mask;
+tmp = load('ve_mask_noroimask.mat');
+m.ve_noroi = tmp.cur_in;
+
 clear tmp;
 
 bsDir = '/Volumes/server/Projects/MEG/brainstorm_db';
@@ -19,3 +24,7 @@ visualizeBrainstormMesh(anatDir, double(m.ve), [],[],[], 'VE mask');
 fprintf('Number of total vertices ROI mask: %d\n', sum(m.roi));
 fprintf('Number of total vertices VE mask: %d\n', sum(m.ve));
 fprintf('Number of vertices overlapping ROI/VE mask: %d\n', length(intersect(find(m.roi),find(m.ve))));
+
+fprintf('Number of total vertices ROI mask: %d\n', sum(m.all));
+fprintf('Number of total vertices VE mask: %d\n', sum(m.ve_noroi));
+fprintf('Number of vertices overlapping ROI/VE mask: %d\n', length(intersect(find(m.all),find(m.ve_noroi))));
