78ca02d086c237697d6b6b0cdd308d6751746bbb
elinekupers <elinekupers@gmail.com>
2019-02-20 19:46:49 -0500

    clean up and deal with new file names

--- analysis/subfunctions/session/s_visualizeROIvsVEmask.m
+++ analysis/subfunctions/session/s_visualizeROIvsVEmask.m
@@ -1,30 +1,36 @@
 % s_visualizeROIvsVEmask
 
-tmp = load('roimask.mat');
-m.roi = tmp.roi.mask;
-tmp = load('ve_mask.mat');
-m.ve = tmp.cur_in;
+subjectSessionDir = '/Volumes/server/Projects/MEG/Retinotopy/Subject_sessions';
+bsDir = '/Volumes/server/Projects/MEG/brainstorm_db';
+subject = 'wlsubj068';
+anatDir = fullfile(bsDir, 'MEG_Retinopy', 'anat', subject);
 
-tmp = load('roimask_noroimask.mat');
-m.all = tmp.roi.mask;
-tmp = load('ve_mask_noroimask.mat');
-m.ve_noroi = tmp.cur_in;
+cd(fullfile(subjectSessionDir, subject, 'mask'))
+
+
+tmp = load('roimask.mat');
+m.roi = double(tmp.cur_in);
+tmp = load('vemask_withroimask.mat');
+m.both = double(tmp.cur_in);
+tmp = load('vemask_noroimask.mat');
+m.ve = double(tmp.cur_in);
 
 clear tmp;
 
-bsDir = '/Volumes/server/Projects/MEG/brainstorm_db';
-subject = 'wlsubj040';
-anatDir = fullfile(bsDir, 'MEG_Retinopy', 'anat', subject);
 
 
-visualizeBrainstormMesh(anatDir, m.roi, [],[],[], 'ROI mask');
-visualizeBrainstormMesh(anatDir, double(m.ve), [],[],[], 'VE mask');
+visualizeBrainstormMesh(anatDir, m.both, [],[],[], 'ROI+VE mask');
+print(gcf,'-dpng', [] ,fullfile(subjectSessionDir, subject, 'mask', 'bs_mesh_roive'));
+savefig(gcf,fullfile(subjectSessionDir, subject, 'mask', 'bs_mesh_roive.fig'));
+
+visualizeBrainstormMesh(anatDir, m.ve, [],[],[], 'VE mask');
+print(gcf,'-dpng', [] ,fullfile(subjectSessionDir, subject, 'mask', 'bs_mesh_ve'));
+savefig(gcf,fullfile(subjectSessionDir, subject, 'mask', 'bs_mesh_ve.fig'));
 
 
-fprintf('Number of total vertices ROI mask: %d\n', sum(m.roi));
+fprintf('Number of total vertices ROI+VE mask: %d\n', sum(m.both));
 fprintf('Number of total vertices VE mask: %d\n', sum(m.ve));
-fprintf('Number of vertices overlapping ROI/VE mask: %d\n', length(intersect(find(m.roi),find(m.ve))));
-
-fprintf('Number of total vertices ROI mask: %d\n', sum(m.all));
-fprintf('Number of total vertices VE mask: %d\n', sum(m.ve_noroi));
-fprintf('Number of vertices overlapping ROI/VE mask: %d\n', length(intersect(find(m.all),find(m.ve_noroi))));
+fprintf('Number of total vertices ROI mask: %d\n', sum(m.roi));
+fprintf('Number of vertices overlapping ROI+VE vs VE only mask: %d\n', length(intersect(find(m.both),find(m.ve))));
+fprintf('Number of vertices overlapping ROI+VE vs ROI only mask: %d\n', length(intersect(find(m.both),find(m.roi))));
+fprintf('Number of vertices overlapping ROI only vs VE only mask: %d\n', length(intersect(find(m.roi),find(m.ve))));
