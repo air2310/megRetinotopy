46e22168d32319f8a998f867066a25a6322d0297
elinekupers <elinekupers@gmail.com>
2019-03-28 12:05:47 -0400

    add script to load FS mesh and dispplay Wang ROIs

--- analysis/subfunctions/session/s_visualizeROIvsVEmask.m
+++ analysis/subfunctions/session/s_visualizeROIvsVEmask.m
@@ -2,7 +2,7 @@
 
 subjectSessionDir = '/Volumes/server/Projects/MEG/Retinotopy/Subject_sessions';
 bsDir = '/Volumes/server/Projects/MEG/brainstorm_db';
-subject = 'wlsubj030';
+subject = 'wlsubj058';
 anatDir = fullfile(bsDir, 'MEG_Retinopy', 'anat', subject);
 
 cd(fullfile(subjectSessionDir, subject, 'mask'))
@@ -38,3 +38,58 @@ fprintf('Number of total vertices ROI mask: %d\n', sum(m.roi));
 fprintf('Number of vertices overlapping ROI+VE vs VE only mask: %d\n', length(intersect(find(m.both),find(m.ve))));
 fprintf('Number of vertices overlapping ROI+VE vs ROI only mask: %d\n', length(intersect(find(m.both),find(m.roi))));
 fprintf('Number of vertices overlapping ROI only vs VE only mask: %d\n', length(intersect(find(m.roi),find(m.ve))));
+
+
+%% Load Freesurfer meshes
+
+% see t_initAnatomyFromFreesurfer, meshImportFreesurferSurfaces
+hemi = 'lh';
+fsHemiPth    = fullfile('/Volumes/server/Freesurfer_subjects/', subject, 'surf', sprintf('%s.inflated',hemi));
+
+% Load mesh from FreeSurfer
+mshFS = fs_meshFromSurface(fsHemiPth);
+
+
+% Load data from Wang Atlas
+wangAtlasPth = fullfile(subjectSessionDir, subject, 'rois', 'surface', 'freesurfer', sprintf('%s.all_rois', hemi));
+[curvWangAtlas, fnum] = read_curv(wangAtlasPth);
+
+% Visualize with vistasoft
+% meshVisualize(mshFS)
+
+
+
+% Visualize with mMatlab
+figure(1)    
+% Faces (also called triangles) are defined by 3 points, each of
+% which is an index into the x, y, z vertices
+faces = mshFS.triangles' + 1; % we need to 1-index rather than 0-index for Matlab
+    
+% The vertices are the locations in mm spacing
+x     = mshFS.vertices(1,:)';
+y     = mshFS.vertices(2,:)';
+z     = mshFS.vertices(3,:)';
+    
+% The colormap will, by default, paint sulci dark and gyri light
+c     = mshFS.colors(1,:)';
+
+nanIdx = isnan(curvWangAtlas);
+roiIdx = find(curvWangAtlas(~nanIdx));
+c(roiIdx) = 255;
+    
+% Render the triangle mesh
+tH = trimesh(faces, x,y,z);
+
+% Make it look nice
+set(tH, 'LineStyle', 'none', 'FaceColor', 'interp', 'FaceVertexCData',c)
+axis equal off; colormap gray; set(gca, 'CLim', [0 255])
+    
+% Lighting to make it look glossy
+light('Position',100*[0 1 1],'Style','local')
+% lighting gouraud
+
+% Which mesh are we plotting?
+title(sprintf('Freesurfer mesh: %s', hemi))
+
+% Rotate it
+set(gca, 'View', [-16.7000  -90.0000]);
