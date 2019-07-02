e8760f69e1ded2bacb8bc7c5e2d28515cf07b768
elinekupers <elinekupers@gmail.com>
2019-07-02 18:59:12 -0400

    add frame of new phase reference function

---
+++ mprf_MEGPhaseReferenceData.m
@@ -0,0 +1,25 @@
+function phaseRefMEGResponse = mprf_MEGPhaseReferenceData(megData, predMEGResponse)
+% Function to computing phase referenced amplitude from preprocessed MEG data 
+% and predicted MEG responses from cortical surface
+%   phaseRefMEGResponse = mprf_MEGPhaseReferenceData(megData, predMEGResponse)
+%   	
+% INPUTS:
+%   megData         : preprocessed MEG data (time x epochs x run x sensors)
+%   predMEGResponse : predicted MEG responses (epochs x sensors)
+%  
+% OUTPUT:
+%   phaseRefMEGResponse : Phase referenced MEG time series (sensors x epochs)
+
+
+
+
+
+
+
+
+
+
+
+return
+
+
