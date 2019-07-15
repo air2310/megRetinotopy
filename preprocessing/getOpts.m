8ada325debc6faae859a3e1cbd93aaf47525c9cc
elinekupers <elinekupers@gmail.com>
2019-07-15 15:20:10 -0400

    add subfolder for original analysis, make sure that if not perturbing prfs, the vary size and position or scrambling iterations is set to empty, also move the visualize surf opt from the main script to here

--- preprocessing/getOpts.m
+++ preprocessing/getOpts.m
@@ -16,6 +16,7 @@ opt.verbose               = true;           % General
 opt.doSaveData            = true;           % General
 opt.saveFig               = true;           % General
 opt.fullSizeGainMtx       = false;          % General: if true, execute analysis with fullsize meshes and gain matrix (FS size), if false, downsample to Brainstorm mesh size
+opt.surfVisualize         = false;          % General: visualize surface meshes
 
 % --- MEG Preproc ---
 opt.doFiltering           = true;           % MEG preprocessing
@@ -40,41 +41,42 @@ opt.useBensonMaps         = false;          % MRI prf model: Make prediction fro
 
 % --- PERTUBATION of pRF models ---
 opt.perturbOrigPRFs       = false;          % PERTUBATION of MRI prf model: say false for none, or choose from 'position', 'size', 'scramble'
-opt.varyPosition          = -pi:(pi/4):pi;  % PERTUBATION of MRI prf model: Vary the prf center (polar angle rotation)
-opt.varySize              = unique(round(logspace(log10(0.2),log10(10),20),1)); % PERTUBATION of MRI prf model: Vary the prf size (scale sigma)
-opt.nScrambles            = 1000;
+if opt.perturbOrigPRFs
+    opt.varyPosition          = -pi:(pi/4):pi;  % PERTUBATION of MRI prf model: Vary the prf center (polar angle rotation)
+    opt.varySize              = unique(round(logspace(log10(0.2),log10(10),20),1)); % PERTUBATION of MRI prf model: Vary the prf size (scale sigma)
+    opt.nScrambles            = 1000;
+    opt.subfolder             = ['vary_' opt.perturbOrigPRFs]; % create subfolder in case of saving images / data
+else
+    opt.varyPosition          = [];
+    opt.varySize              = [];
+    opt.nScrambles            = [];
+    opt.subfolder             = 'original'; 
+end
 
 % --- Folders and filenames ---
 opt.fNamePostFix          = sprintf('_benson%d_highres%d_smoothed%d', ...
-                            opt.useBensonMaps, opt.fullSizeGainMtx, opt.useSmoothedData);
+    opt.useBensonMaps, opt.fullSizeGainMtx, opt.useSmoothedData);
 
-if opt.saveFig || opt.doSaveData
-    if opt.perturbOrigPRFs 
-        opt.subfolder = ['vary_' opt.perturbOrigPRFs]; 
-    else subfolder = []; 
+
+%% Check for extra inputs in case changing the default options
+if exist('varargin', 'var')
+    
+    % Get fieldnames
+    fns = fieldnames(opt);
+    for ii = 1:2:length(varargin)
+        % paired parameter and value
+        parname = varargin{ii};
+        val     = varargin{ii+1};
+        
+        % check whether this parameter exists in the defaults
+        idx = cellfind(fns, parname);
+        
+        % if so, replace it; if not add it to the end of opt
+        if ~isempty(idx), opt.(fns{idx}) = val;
+        else, opt.(parname) = val; end
+        
+        
     end
 end
 
-
-% Check for extra inputs in case changing the default options
-if exist('varargin', 'var')
-   
-    % Get fieldnames
-    fns = fieldnames(opt);
-     for ii = 1:2:length(varargin)
-         % paired parameter and value
-         parname = varargin{ii};
-         val     = varargin{ii+1};
-         
-         % check whether this parameter exists in the defaults
-         idx = cellfind(fns, parname);
-         
-         % if so, replace it; if not add it to the end of opt
-         if ~isempty(idx), opt.(fns{idx}) = val;
-         else, opt.(parname) = val; end
-            
-            
-     end
-end
-    
 return
