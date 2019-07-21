7fc918ab60e1f2d3dcb69facbb0ab38670a184cf
elinekupers <elinekupers@gmail.com>
2019-07-21 12:40:21 -0400

    make all figures from this separate function, instead of from main analysis pipeline

---
+++ figurescripts/makeAllFigures.m
@@ -0,0 +1,21 @@
+function makeAllFigures(subjID)
+
+% Load paths with data files for this subject
+dirPth = loadPaths(subjID);
+
+% Go back to root
+cd(mprf_rootPath)
+
+% Set options
+opt = getOpts;
+
+%% Figures
+
+% Figure 1. Time series (1A) and MEG head plot (1B)
+makeFigure1(dirPth,opt);
+
+%Figure 2. Position range line plot and headplots for every position range
+makeFigure2(dirPth,opt);
+
+%Figure 3. Size range line plot and headplots for every size range
+makeFigure3(dirPth,opt);
