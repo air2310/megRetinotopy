7db3d5bf361b8a159fca66f470935c7e474b957a
elinekupers <elinekupers@gmail.com>
2019-09-23 12:30:33 -0400

    resolve merge conflict

--- figurescripts/makeFigure1A.m
+++ figurescripts/makeFigure1A.m
@@ -1,6 +1,6 @@
 function makeFigure1A(dirPth,opt)
 % Function to create figure 1A (MEG head plot showing the variance
-% explained values for individual subjects). 
+% explained values for individual subjects).
 
 varExpFile = dir(fullfile(dirPth.model.saveDataPth,'original','coherent','pred_resp','meanVarExpl.mat'));
 
@@ -10,20 +10,24 @@ if ~exist(saveDir,'dir')
     mkdir(fullfile(dirPth.finalFig.savePth,'figure1',saveSubDir));
 end
 
-% check if the modelPredictions are saved in the folder. Else run
-% mprf_main.m
+% check if the modelPredictions are saved in the folder.
 close all;
 if ~isempty(varExpFile)
-    load(fullfile(varExpFile.folder,varExpFile.name),'meanVarExpl');
-<<<<<<< HEAD
-    
-    fH1 = figure; megPlotMap(meanVarExpl,[0 0.45],fH1, 'parula', 'mean variance explained', [],[]);
-=======
-    clims = [0 max(meanVarExpl)];
-    fH1 = figure; megPlotMap(meanVarExpl,clims,fH1, 'parula', 'mean variance explained', [],[], 'interpmethod', 'nearest');
->>>>>>> b8ccd3008cb66c2475368e6460d62ddbc36101b5
-    c = colorbar; c.Location='southoutside';    
+    error('Can''t find mean variance explained mat-file')
 end
+
+% Load the variance explained file
+load(fullfile(varExpFile.folder,varExpFile.name),'meanVarExpl');
+
+% Set colormap limits
+clims = [0 max(meanVarExpl)];
+
+% Plot it!
+fH1 = figure;
+megPlotMap(meanVarExpl,clims,fH1, 'parula', ...
+    'mean variance explained', [],[], 'interpmethod', 'nearest');
+c = colorbar; c.Location='southoutside';
+
 
 if opt.saveFig
     print(fH1, fullfile(saveDir, sprintf('Mean_variance_explained')), '-dpng');
