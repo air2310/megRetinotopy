dfa4ac968e03d2b02d4a8f668fa1df3af6922834
elinekupers <elinekupers@gmail.com>
2019-11-18 17:01:03 -0500

    plot individual subjects in one figure

--- figurescripts/makeFigure1C.m
+++ figurescripts/makeFigure1C.m
@@ -4,35 +4,42 @@ function makeFigure1C(subjIDs,opt)
 
 % define initial parameters
 numChans = length(opt.meg.dataChan);
-numSub = length(subjIDs);
+numSub   = length(subjIDs);
 varExplAllSubj = nan(numSub,numChans);
+
+% Plot all individuals
+fH1 = figure; set(gcf,'Position',[1000, 651, 1500, 687]);
 
 % loop over all subjects
 for idxSubj = 1:numSub
-    % calculate average variance explained across subjects
     
     % Load paths with data files for this subject
     dirPth = loadPaths(subjIDs{idxSubj});
     
     varExpFile = dir(fullfile(dirPth.model.saveDataPth,opt.subfolder,'pred_resp','meanVarExpl.mat'));
     if ~isempty(varExpFile)
-        load(fullfile(varExpFile.folder,varExpFile.name),'meanVarExpl');
-        
-        varExplAllSubj(idxSubj,:) = meanVarExpl;
-        
+        load(fullfile(varExpFile.folder,varExpFile.name),'meanVarExpl');       
+        varExplAllSubj(idxSubj,:) = meanVarExpl;      
     end
-     
+    
+    % plot mesh of subject
+    subplot(2,5,idxSubj);
+    ttl = sprintf('S%d', idxSubj);
+    megPlotMap(varExplAllSubj(idxSubj,:),[0 0.45], fH1, 'parula', ttl,[],[],'interpmethod', 'nearest'); hold on;
+    c = colorbar; c.TickDirection = 'out'; c.Box = 'off';
+    pos = c.Position; set(c, 'Position', [pos(1)+0.04 pos(2)+0.03, pos(3)/1.5, pos(4)/1.5])
+    
 end
 
+% calculate average variance explained across subjects
 meanVarExplAllSubj    = nanmean(varExplAllSubj,1);
 [maxMeanVarExplAllSubj, maxSen] = nanmax(meanVarExplAllSubj);
 
-close all;
-
-fH1 = figure; set(gcf,'Position',[100 100 1920/2 1920/2]);
-ttl = strcat('Mean variance explained (Average across subjects: N=', sprintf('%d )',numSub));
-megPlotMap(meanVarExplAllSubj,[0 maxMeanVarExplAllSubj],fH1, 'parula', ttl,[],[],'interpmethod', 'nearest');
-c = colorbar; c.Location='southoutside';
+% Plot average
+fH2 = figure; set(gcf,'Position',[100 100 1920/2 1920/2]);
+ttl = strcat('Mean variance explained (Group Average N=', sprintf('%d)',numSub));
+megPlotMap(meanVarExplAllSubj,[0 0.30],fH2, 'parula', ttl,[],[],'interpmethod', 'nearest');
+c = colorbar; c.TickDirection = 'out'; c.Box = 'off';
 
 if opt.saveFig
     saveSubDir = 'figure1C';
@@ -41,11 +48,14 @@ if opt.saveFig
     if ~exist(saveDir,'dir')
         mkdir(saveDir);
     end
+     
+    print(fH1, fullfile(saveDir, sprintf('fig1D_Individual_subjecs_VE_%s', opt.fNamePostFix)), '-dpng');
+    print(fH1, fullfile(saveDir, sprintf('fig1D_Individual_subjecs_VE_%s', opt.fNamePostFix)), '-depsc');
+    fprintf('\n saving figure 1D in %s',saveDir);
     
-    print(fH1, fullfile(saveDir, sprintf('Mean_VE_average_%d_subjs_maxVar_%2.0f_sen_%d',numSub,100*maxMeanVarExplAllSubj,maxSen)), '-dpng');
-    print(fH1, fullfile(saveDir, sprintf('Mean_VE_average_%d_subjs_maxVar_%2.0f_sen_%d',numSub,100*maxMeanVarExplAllSubj,maxSen)), '-depsc');
-    fprintf('\n saving figure 1A in %s',saveDir);
-    
+    print(fH2, fullfile(saveDir, sprintf('fig1C_Mean_VE_average_%d_subjs_maxVar_%2.0f_sen_%d_%s',numSub,100*maxMeanVarExplAllSubj,maxSen, opt.fNamePostFix)), '-dpng');
+    print(fH2, fullfile(saveDir, sprintf('fig1C_Mean_VE_average_%d_subjs_maxVar_%2.0f_sen_%d_%s',numSub,100*maxMeanVarExplAllSubj,maxSen, opt.fNamePostFix)), '-depsc');
+    fprintf('\n saving figure 1C in %s',saveDir);
     
     fprintf('\n');
 end
