668b1497827d4190ff20f6cda11b7d03e62d5c10
elinekupers <elinekupers@gmail.com>
2020-02-07 12:04:06 -0500

    do not model offset in regression

--- StimRefFwdMdl_analysis/regressPredictedResponse.m
+++ StimRefFwdMdl_analysis/regressPredictedResponse.m
@@ -1,8 +1,10 @@
 function [B, varexpl] = regressPredictedResponse(data, prediction)
 
 % Get design matrix (add column of ones to subtract out the mean)
-X = [ones(size(prediction)) prediction];
-            
+% X = [ones(size(prediction)) prediction];
+X = prediction; % do not model offset, because we assume that a blank stimulus should give a 0 response across trials.
+
+
 % Regress prediction from phase referenced 10 Hz MEG response
 [B,~,~,~,stats] = regress(data,X);
             
