931150cd1ac261a706d38d88db0f3a68286080d0
elinekupers <elinekupers@gmail.com>
2020-03-25 11:01:46 -0400

    rename figurescript 1c into supplement, because it only makes supplemental figs now we use the average modelfit as average subject result

--- figurescripts/makeFigure1Supplement.m
+++ figurescripts/makeFigure1Supplement.m
@@ -1,4 +1,4 @@
-function makeFigure1C(subjIDs)
+function makeFigure1Supplement(subjIDs,saveSubDir)
 % Function to create figure 1A (MEG head plot showing the variance
 % explained values for individual subjects).
 
@@ -8,7 +8,8 @@ numChans = 157;
 numSub   = length(subjIDs);
 varExplAllSubj = nan(numSub,numChans);
 
-maxClim = false;
+maxClim = true;
+interpMethod =  'nearest';
 
 % Plot all individuals
 fH1 = figure; set(gcf,'Position',[1000, 651, 1500, 687]);
@@ -36,7 +37,7 @@ for idxSubj = 1:numSub
     % plot mesh of subject
     subplot(2,5,idxSubj);
     ttl = sprintf('S%d', idxSubj);
-    megPlotMap(varExplAllSubj(idxSubj,:),[0 cmax], fH1, 'parula', ttl,[],[],'interpmethod', []); hold on;
+    megPlotMap(varExplAllSubj(idxSubj,:),[0 cmax], fH1, 'parula', ttl,[],[],'interpmethod', interpMethod); hold on;
     c = colorbar; c.TickDirection = 'out'; c.Box = 'off';
     pos = c.Position; set(c, 'Position', [pos(1)+0.04 pos(2)+0.03, pos(3)/1.5, pos(4)/1.5])
     
@@ -55,13 +56,12 @@ end
 % Plot average
 fH2 = figure; set(gcf,'Position',[100 100 1920/2 1920/2]);
 ttl = strcat('Mean variance explained (Group Average N=', sprintf('%d)',numSub));
-megPlotMap(meanVarExplAllSubj,[0 cmax],fH2, 'parula', ttl,[],[],'interpmethod', []);
+megPlotMap(meanVarExplAllSubj,[0 cmax],fH2, 'parula', ttl,[],[],'interpmethod', interpMethod);
 c = colorbar; c.TickDirection = 'out'; c.Box = 'off';
 
 
 if opt.saveFig
-    saveSubDir = 'figure1C';
-    saveDir = fullfile(mprf_rootPath,'data','Retinotopy','Quality_check','average','finalfig',saveSubDir);
+    saveDir = fullfile(mprf_rootPath,'data','Retinotopy','Quality_check','average','finalfig',saveSubDir, 'figure1C');
     
     if ~exist(saveDir,'dir')
         mkdir(saveDir);
@@ -74,13 +74,13 @@ if opt.saveFig
     end
         
     figure(fH1);
-    figurewrite(fullfile(saveDir, sprintf('fig1D_Individual_subjecs_VE_%s%s', opt.fNamePostFix,figNameEnd)),[],[1 300],'.',1);
-    figurewrite(fullfile(saveDir, sprintf('fig1D_Individual_subjecs_VE_%s%s', opt.fNamePostFix,figNameEnd)),[],0,'.',1);
+    figurewrite(fullfile(saveDir, sprintf('fig1Supplement_Individual_subjecs_VE_%s%s', opt.fNamePostFix,figNameEnd)),[],[1 300],'.',1);
+    figurewrite(fullfile(saveDir, sprintf('fig1Supplement_Individual_subjecs_VE_%s%s', opt.fNamePostFix,figNameEnd)),[],0,'.',1);
     fprintf('\n saving figure 1D in %s',saveDir);
     
     figure(fH2);
-    figurewrite(fullfile(saveDir, sprintf('fig1C_Mean_VE_average_%d_subjs_maxVar_%2.0f_sen_%d_%s%s',numSub,100*maxMeanVarExplAllSubj,maxSen, opt.fNamePostFix,figNameEnd)),[],[1 300],'.',1);
-    figurewrite(fullfile(saveDir, sprintf('fig1C_Mean_VE_average_%d_subjs_maxVar_%2.0f_sen_%d_%s%s',numSub,100*maxMeanVarExplAllSubj,maxSen, opt.fNamePostFix,figNameEnd)),[],0,'.',1);
+    figurewrite(fullfile(saveDir, sprintf('fig1Supplement_SensorwiseAverage_%d_subjs_maxVar_%2.0f_sen_%d_%s%s',numSub,100*maxMeanVarExplAllSubj,maxSen, opt.fNamePostFix,figNameEnd)),[],[1 300],'.',1);
+    figurewrite(fullfile(saveDir, sprintf('fig1Supplement_SensorwiseAverage_%d_subjs_maxVar_%2.0f_sen_%d_%s%s',numSub,100*maxMeanVarExplAllSubj,maxSen, opt.fNamePostFix,figNameEnd)),[],0,'.',1);
     fprintf('\n saving figure 1C in %s',saveDir);
     
     fprintf('\n');
