e4592075f9214d9d2069ec45fac6a24c865c0922
elinekupers <elinekupers@gmail.com>
2020-03-25 11:30:19 -0400

    regression function can now handle if you want to fit with or without offset parameter and outputs those params accordingly

--- StimRefFwdMdl_analysis/regressPredictedResponse.m
+++ StimRefFwdMdl_analysis/regressPredictedResponse.m
@@ -1,13 +1,32 @@
-function [B, varexpl] = regressPredictedResponse(data, prediction)
+function [betas, offsets, varexpl] = regressPredictedResponse(data, prediction, varargin)
 
-% Get design matrix (add column of ones to subtract out the mean)
-% X = [ones(size(prediction)) prediction];
-X = prediction; % do not model offset, because we assume that a blank stimulus should give a 0 response across trials.
+% check inputs
+if nargin < 3
+    regressionType = 'NoOffset';
+else
+    regressionType = varargin{2};
+end
+    
 
+% Get design matrix
+if strcmp(regressionType, 'NoOffset')
+    X = prediction; % do not model offset, because we assume that a blank stimulus should give a 0 response across trials.
+elseif strcmp(regressionType, 'WithOffset')
+    X = [ones(size(prediction)), prediction];
+end
 
 % Regress prediction from phase referenced 10 Hz MEG response
 [B,~,~,~,stats] = regress(data,X);
-            
+
+if strcmp(regressionType, 'NoOffset')
+    betas   = B(1);
+    offsets = [];
+elseif strcmp(regressionType, 'WithOffset')
+    offsets = B(1);
+    betas = B(2);
+end
+    
 varexpl = stats(1);
+
 
 return
