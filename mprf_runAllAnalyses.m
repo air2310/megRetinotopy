2dfecfb84e0b6d2e4376b468b002f6c64611caf6
elinekupers <elinekupers@gmail.com>
2020-03-31 20:21:22 -0400

    incorporate option to recompute betas again

--- mprf_runAllAnalyses.m
+++ mprf_runAllAnalyses.m
@@ -1,21 +1,22 @@
 % Define all subjects
 subjects = {'wlsubj004', 'wlsubj039', 'wlsubj040', 'wlsubj058','wlsubj068', ...
-            'wlsubj070', 'wlsubj081', 'wlsubj106', 'wlsubj109', 'wlsubj111'};
+            'wlsubj070', 'wlsubj081', 'wlsubj106', 'wlsubj109', 'wlsubj111'}; % 
      
-rng(1)
 
 % Original
-opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs',false); % see getOpts function for more options
+opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs', false, 'recomputeFinalPredictionBetas', true); % see getOpts function for more options
+
+% reset random number generator seed for reproducibility
+rng(opt.seed)
 for s = 1:length(subjects)
     subjID = subjects{s};
-    
-    
+   
     mprf_main(subjID, opt);
     close all
 end
 
 % Size
-opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs','size'); % see getOpts function for more options
+opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs','size', 'recomputeFinalPredictionBetas', true); % see getOpts function for more options
 for s = 1:length(subjects)
     subjID = subjects{s};  
     mprf_main(subjID, opt);
@@ -23,7 +24,7 @@ for s = 1:length(subjects)
 end
 
 % Position
-opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs','position'); % see getOpts function for more options
+opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs','position','recomputeFinalPredictionBetas', true); % see getOpts function for more options
 for s = 1:length(subjects)
     subjID = subjects{s};
     mprf_main(subjID, opt);
@@ -41,7 +42,7 @@ close all; makeAllFigures(subjID, 1:3, 'top10', true, 'meanVE');
 
 
 % % Scramble
-% opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs','scramble'); % see getOpts function for more options
+% opt = getOpts('saveFig',1,'verbose',0, 'fullSizeMesh', 1, 'perturbOrigPRFs','scramble', 'recomputeFinalPredictionBetas', true); % see getOpts function for more options
 % for s = 1:length(subjects)
 %     subjID = subjects{s};
 %     mprf_main(subjID, opt);
