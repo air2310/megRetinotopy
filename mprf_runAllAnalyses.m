e30d7c3b22dafd61a278bc17b8fb8a0e2d36809a
elinekupers <elinekupers@gmail.com>
2020-09-10 16:23:55 -0400

    clean up, only do manuscript analysis without offset and recomputation of betas

--- mprf_runAllAnalyses.m
+++ mprf_runAllAnalyses.m
@@ -1,85 +1,76 @@
 % mprf_runAllAnalysis.m
-% 
-% Master script to run 4 types of analyses in the MEG retinotopy project.
-% The script runs the modelfits with either 2 free parameters (reference
-% phase and gain, 'NoOffset') or 3 free parameters (reference phase, gain
-% and offset, 'WithOffset')
+%
+% Master script to run 3 types of analyses in the MEG retinotopy project.
+% Default script runs the modelfits with 2 free parameters (beta (or gain)
+% and reference phase. Model uses split-half cross-validation to get the 
+% the final reference phase and betas within a subject.
+%
+% Previous versions of the model could add an additional offset, 
+% ('addOffsetParam') or refits gain parameters (so only cross-validate
+% reference phase ('refitGainParam'). But this model fit can be subject to
+% overfitting and is therefore not recommended.
 
 % Define all subjects
 subjects = {'wlsubj004', 'wlsubj039', 'wlsubj040', 'wlsubj058','wlsubj068', ...
     'wlsubj070', 'wlsubj081', 'wlsubj106', 'wlsubj109', 'wlsubj111'}; %
 
-addOffsetParam  = [false, true];
-refitGainParam  = [false, true];
+% Define additional free params / options
+addOffsetParam  = false;
+refitGainParam  = false;
 
-for rt = 1:length(addOffsetParam)
-      offset = addOffsetParam(rt);
-    
-    for rb = 1:length(refitGainParam)
-        refitGain = refitGainParam(rb);
-        
-        
-        % With fMRI pRF parameters
-        opt = getOpts('saveFig', true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs', false, ...
-            'addOffsetParam', offset, 'refitGainParam', refitGain); % see getOpts function for more options          
-        
-        for s = 1:length(subjects)
-            subjID = subjects{s};
-            mprf_main(subjID, opt);
-        end
-        
-        % With altered pRF size
-        opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs','size', ...
-            'addOffsetParam', offset, 'refitGainParam', refitGain); % see getOpts function for more options
-        for s = 1:length(subjects)
-            subjID = subjects{s};
-            mprf_main(subjID, opt);
-            close all
-        end
-        
-        % With rotated pRF position
-        opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs','position', ...
-            'addOffsetParam', offset, 'refitGainParam', refitGain); % see getOpts function for more options
-        for s = 1:length(subjects)
-            subjID = subjects{s};
-            mprf_main(subjID, opt);
-            close all
-        end
-        
-        
+
+% With fMRI pRF parameters
+opt = getOpts('saveFig', true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs', false, ...
+    'addOffsetParam', addOffsetParam, 'refitGainParam', refitGainParam); % see getOpts function for more options
+
+for s = 1:length(subjects)
+    subjID = subjects{s};
+    mprf_main(subjID, opt);
+end
+
+% With altered pRF size
+opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs','size', ...
+    'addOffsetParam', addOffsetParam, 'refitGainParam', refitGainParam); % see getOpts function for more options
+for s = 1:length(subjects)
+    subjID = subjects{s};
+    mprf_main(subjID, opt);
+    close all
+end
+
+% With rotated pRF position
+opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs','position', ...
+    'addOffsetParam', addOffsetParam, 'refitGainParam', refitGainParam); % see getOpts function for more options
+for s = 1:length(subjects)
+    subjID = subjects{s};
+    mprf_main(subjID, opt);
+    close all
+end
+
+
 %       % Scramble
 %       opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs','scramble', ...
-%           'addOffsetParam', addOffsetParam, 'refitGainParam', refitGain); % see getOpts function for more options
+%           'addOffsetParam', addOffsetParam, 'refitGainParam', refitGainParam); % see getOpts function for more options
 %       for s = 1:length(subjects)
 %           subjID = subjects{s};
 %           mprf_main(subjID, opt);
 %           close all
 %       end
-        
-    end
-end
+
 
 
 %% Visualize all types of analyses
-for rt = 1:length(addOffsetParam)
-    offset = addOffsetParam(rt);
-    
-    for rb = 1:length(refitGainParam)
-        refitGain = refitGainParam(rb);
-        
-        
-        % Make individual subject figures
-        opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs', false,...
-            'addOffsetParam', offset, 'refitGainParam', refitGain); % see getOpts function for more options
-        
-        for s = 1:length(subjects)
-            subjID = subjects{s};
-            close all; makeAllFigures(subjID, 1:3, 'top10', false, 'meanVE', opt);
-        end
-        
-        % Make average figures
-        close all; makeAllFigures('wlsubj004', 1:3, 'top10', true, 'meanVE', opt);
-    end
+
+% Make individual subject figures
+opt = getOpts('saveFig',true,'verbose',false, 'fullSizeMesh', true, 'perturbOrigPRFs', false,...
+    'addOffsetParam', addOffsetParam, 'refitGainParam', refitGainParam); % see getOpts function for more options
+
+for s = 1:length(subjects)
+    subjID = subjects{s};
+    close all; makeAllFigures(subjID, 1:3, 'top10', false, 'meanVE', opt);
 end
+
+% Make average figures
+close all; makeAllFigures('wlsubj004', 1:3, 'top10', true, 'meanVE', opt);
+
 
 return
